bccc43cedc9bb482d2bded5a981c139ff36d1040
Daniel Petri Rocha <daniel.petri@tum.de>
2022-03-15 17:29:41 +0100

    Adds 'current' as optional parameter in the upload endpoint

--- bigbluebutton-config/bin/apply-lib.sh
+++ bigbluebutton-config/bin/apply-lib.sh
@@ -105,7 +105,7 @@ enableUFWRules() {
 
 
 enableMultipleKurentos() {
-  echo "  - Configuring three Kurento Media Servers (listen only, webcam, and screenshare)"
+  echo "  - Configuring three Kurento Media Servers (listen only, webcam, and screeshare)"
 
   # Step 1.  Setup shared certificate between FreeSWITCH and Kurento
 
